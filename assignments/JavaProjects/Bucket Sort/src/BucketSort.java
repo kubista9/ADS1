07f42b9addc4d830630afd06366d6dbcd1b552d0
Jakub Kuka <jakubkuka07@gmail.com>
2024-03-11 20:24:38 +0100

    Implemented the generateEvenlyDistributedArray for testing

--- assignments/JavaProjects/Bucket Sort/src/BucketSort.java
+++ assignments/JavaProjects/Bucket Sort/src/BucketSort.java
@@ -1,6 +1,38 @@
 import java.util.*;
 
 public class BucketSort {
+
+	public static ArrayList<int[]> generateEvenlyDistributedArray(int numberOfArrays) {
+		ArrayList<int[]> arrayListOfArrays = new ArrayList<>();
+
+		for (int i = 0; i < numberOfArrays; i++) {
+			int[] randomArray = generateRandomArray();
+			int randomArraySize = randomArray.length;
+			Random randomElement = new Random();
+
+			// Fill the random array with random elements
+			for (int j = 0; j < randomArraySize; j++) {
+				int randomCoefficient = 3;
+				int coefficient = randomElement.nextInt(randomCoefficient);
+				randomArray[j] = j + coefficient;
+			}
+			arrayListOfArrays.add(randomArray);
+		}
+
+		for (int[] array : arrayListOfArrays) {
+			System.out.println("Content of the arrayList: " + Arrays.toString(array));
+		}
+		return arrayListOfArrays;
+	}
+
+	public static int[] generateRandomArray() {
+		Random randomSize = new Random();
+		int a = 30;
+		int randomArraySize = randomSize.nextInt(a) + 1;
+		int[] randomArray = new int[randomArraySize];
+		return randomArray;
+	}
+
 	public static void sort(int[] array, int bucketSize) {
 		int[] bucket = new int[bucketSize + 1];
 
@@ -8,13 +40,10 @@ public class BucketSort {
 			bucket[array[i]]++;
 		}
 
-		// tracks the position in the original array where
-		// the next sorted element should be placed
-		int currentSortPosition = 0;
+		int currentSortPosition = 0; // tracks the position in the original array where the next sorted element should be placed
 		//outer loop - iterates through each index of the bucket array
 		for (int i = 0; i < bucket.length; i++) {
-			//inner loop - for each index in the bucket array represents
-			//how many times the value i is present in the original array
+			//inner loop - for each index in the bucket array represents how many times the value i is present in the original array
 			for (int j = 0; j < bucket[i]; j++) {
 				array[currentSortPosition++] = i;
 			}
@@ -22,11 +51,19 @@ public class BucketSort {
 	}
 
 	public static void main(String[] args) {
-		int[] data = {5, 3, 0, 2, 4, 1, 0, 5, 2, 3, 1, 4}; // Example input
-		System.out.println("Original Array: " + Arrays.toString(data));
+//		int[] unevenlyDistributedArray = {20, 0, 40, 60, 80, 5, 25, 45, 65, 85, 10, 30, 50, 70, 90, 15, 35, 55, 75, 95};
+//		System.out.println("Original unevenly distributed Array: " + Arrays.toString(unevenlyDistributedArray));
+//		int unevenMaxValue = Arrays.stream(unevenlyDistributedArray).max().getAsInt();
+//		sort(unevenlyDistributedArray, unevenMaxValue);
+//		System.out.println("Sorted unevenly distributed array: " + Arrays.toString(unevenlyDistributedArray));
+//
+//		int[] evenlyDistributedArray = {203, 3, 51, 10, 200, 77, 202, 10, 204, 76, 2, 10, 3, 50, 4, 75, 201, 78, 21, 20};
+//		System.out.println("Original evenly distributed Array: " + Arrays.toString(evenlyDistributedArray));
+//		int evenMaxValue = Arrays.stream(evenlyDistributedArray).max().getAsInt();
+//		sort(evenlyDistributedArray, evenMaxValue);
+//		System.out.println("Sorted evenly distributed Array: " + Arrays.toString(evenlyDistributedArray));
 
-		int maxVal = Arrays.stream(data).max().getAsInt();
-		sort(data, maxVal);
-		System.out.println("Sorted Array: " + Arrays.toString(data));
+		generateEvenlyDistributedArray(3);
 	}
 }
+
