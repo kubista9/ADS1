89ea20440e0a03657f6889b698000c807ccd034f
Jakub Kuka <jakubkuka07@gmail.com>
2024-04-22 18:34:18 +0200

    Sudoku -whole project

--- meterial/Assignment3-ADS1/src/Dijkstra.java
+++ meterial/Assignment3-ADS1/src/Dijkstra.java
@@ -1,23 +1,31 @@
 import java.util.*;
 
 class Dijkstra {
+
+	// Initially, the distance to the source node is set to 0, and the distance to all other nodes is set to infinity.
 	public static void dijkstra(Graph graph, String startNode) {
 		Map<String, Integer> distances = new HashMap<>();
 		Map<String, String> previousNodes = new HashMap<>();
+
+		// to select the next node to visit based on the shortest distance. This queue is initialized with the source node.
 		PriorityQueue<String> queue = new PriorityQueue<>(Comparator.comparingInt(distances::get));
 
-		for (String node : graph.adjacencyList.keySet()) {
+		// initializes the distances from the source node to all other nodes in the graph to Integer.MAX_VALUE - infinity
+		for (String node : graph.adjacencyList.keySet()) { //graph.adjacencyList.keySet() method retrieves all the nodes in the graph
 			distances.put(node, Integer.MAX_VALUE);
 		}
-		distances.put(startNode, 0);
-		queue.add(startNode);
+		distances.put(startNode, 0); //sets the distance from the start node to itself as 0
+		queue.add(startNode); //adds the start node to the priority queue. Priority queue is used to select the next node to visit based on the shortest distance
 
+		// It repeatedly selects the node with the smallest distance from the priority queue, updates the
+		// distances of its adjacent nodes, and adds them to the priority queue. This process continues
+		// until the priority queue is empty, indicating that all reachable nodes have been visited.
 		while (!queue.isEmpty()) {
-			String currentNode = queue.poll();
+			String currentNode = queue.poll(); // Removes the node with the smallest distance from the priority queue. Returns the head of the queue.
 			for (Graph.Edge edge : graph.getAdjacentNodes(currentNode)) {
 				int newDistance = distances.get(currentNode) + edge.weight;
 				if (newDistance < distances.get(edge.node)) {
-					distances.put(edge.node, newDistance);
+					distances.put(edge.node, newDistance); //updates the previousNodes map, which keeps track of the node that leads to the shortest path to each node.
 					previousNodes.put(edge.node, currentNode);
 					queue.add(edge.node);
 				}
--- meterial/Assignment3-ADS1/src/Graph.java
+++ meterial/Assignment3-ADS1/src/Graph.java
@@ -4,20 +4,24 @@ class Graph {
 	public Map<String, List<Edge>> adjacencyList;
 
 	public Graph() {
-		this.adjacencyList = new HashMap<>();
+		// Adjacency list - list of nodes that are directly connected to a given node.
+		// The key is a unique identifier for each value in the map. In the context of a graph, the
+		// key represents a node, and the value represents list of nodes that are directly connected to the key node.
+		this.adjacencyList = new HashMap<>(); //for nodes - represented sa keys in the HashMap
 	}
 
+	// When a new node is added to the graph, it is first checked if the node already exists in the HashMap.
+	// If not, a new entry is created with the node as the key and an empty list as the value.
 	public void addNode(String node) {
 		if (!adjacencyList.containsKey(node)) {
 			adjacencyList.put(node, new ArrayList<>());
 		}
 	}
 
-	public void addEdge(String node1, String node2, int weight) {
+	public void addEdge(String node1, String node2, int weight) { //between two nodes
 		addNode(node1);
 		addNode(node2);
 		adjacencyList.get(node1).add(new Edge(node2, weight));
-		adjacencyList.get(node2).add(new Edge(node1, weight)); // For undirected graph
 	}
 
 	public List<Edge> getAdjacentNodes(String node) {
